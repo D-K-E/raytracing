6c0727f4733cc3967fc5d1383a31e2a0ec9e7a21
Qm Auber <qm.auber@tutanota.com>
2020-05-22 05:31:39 +0200

    reference hittables produces black screen as well

--- include/custom/nihai/color.hpp
+++ include/custom/nihai/color.hpp
@@ -1,37 +1,33 @@
 #ifndef COLOR_HPP
 #define COLOR_HPP
 
-#include <custom/sonraki/commons.hpp>
+#include <custom/nihai/commons.hpp>
 #include <iostream>
 
-int cast_color(double d) {
-  // renkleri double'dan int tipine donustur
-  return static_cast<int>(255.999 * d);
-}
-int cast_color(double d, bool isClamped) {
-  // renkleri double'dan int tipine donustur
-  return static_cast<int>(256 * clamp(d, 0.0, 0.999));
-}
+void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
+  auto r = pixel_color.x;
+  auto g = pixel_color.y;
+  auto b = pixel_color.z;
 
-void write_color(std::ostream &out, color pcolor) {
-  out << cast_color(pcolor.x) << ' ' << cast_color(pcolor.y) << ' '
-      << cast_color(pcolor.z) << std::endl;
-}
-void write_color(std::ostream &out, color pcolor, int samples_per_pixel) {
-  double r, g, b;
-  r = pcolor.x;
-  g = pcolor.y;
-  b = pcolor.z;
-  // replace nans with zero since no nans are equal to each other
-  r = (r != r) ? 0.0 : r;
-  g = (g != g) ? 0.0 : g;
-  b = (b != b) ? 0.0 : b;
-  // scale sample
-  r = sqrt(r * 1.0 / samples_per_pixel);
-  g = sqrt(g * 1.0 / samples_per_pixel);
-  b = sqrt(b * 1.0 / samples_per_pixel);
-  out << cast_color(r, true) << ' ' << cast_color(g, true) << ' '
-      << cast_color(b, true) << std::endl;
+  // Replace NaN components with zero. See explanation in Ray Tracing: The Rest
+  // of Your Life.
+  if (r != r)
+    r = 0.0;
+  if (g != g)
+    g = 0.0;
+  if (b != b)
+    b = 0.0;
+
+  // Divide the color by the number of samples and gamma-correct for gamma=2.0.
+  auto scale = 1.0 / samples_per_pixel;
+  r = sqrt(scale * r);
+  g = sqrt(scale * g);
+  b = sqrt(scale * b);
+
+  // Write the translated [0,255] value of each color component.
+  out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+      << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+      << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << std::endl;
 }
 
 #endif
