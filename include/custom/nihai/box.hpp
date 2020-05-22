9b5c78d6d6166c27e28d40c02f244dd51b726885
Qm Auber <qm.auber@tutanota.com>
2020-05-22 03:05:12 +0200

    reference box produces same output but with more sparkles

--- include/custom/nihai/box.hpp
+++ include/custom/nihai/box.hpp
@@ -3,40 +3,44 @@
 //
 #include <custom/nihai/commons.hpp>
 #include <custom/nihai/hittables.hpp>
-#include <custom/nihai/material.hpp>
 //
 #include <custom/nihai/aarect.hpp>
 //
 
 class Box : public Hittable {
 public:
-  point3 bmin;
-  point3 bmax;
-  HittableList sides;
+  Box() {}
+  Box(const point3 &p0, const point3 &p1, shared_ptr<Material> ptr);
 
-public:
-  Box(){};
-  Box(const point3 &p0, const point3 &p1, shared_ptr<Material> ptr)
-      : bmin(p0), bmax(p1) {
-    //
-    sides.add(make_shared<XYRect>(p0.x, p1.x, p0.y, p1.y, p1.z, ptr));
-    sides.add(make_shared<XYRect>(p0.x, p1.x, p0.y, p1.y, p0.z, ptr));
+  virtual bool hit(const Ray &r, double t0, double t1, HitRecord &rec) const;
 
-    sides.add(make_shared<XZRect>(p0.x, p1.x, p0.z, p1.z, p1.y, ptr));
-    sides.add(make_shared<XZRect>(p0.x, p1.x, p0.z, p1.z, p0.y, ptr));
-
-    sides.add(make_shared<YZRect>(p0.y, p1.y, p0.z, p1.z, p1.x, ptr));
-    sides.add(make_shared<YZRect>(p0.y, p1.y, p0.z, p1.z, p0.x, ptr));
-  }
-  bool hit(const Ray &r, double t0, double t1, HitRecord &rec) const {
-    return sides.hit(r, t0, t1, rec);
-  }
   virtual bool bounding_box(double t0, double t1, Aabb &output_box) const {
-    output_box = Aabb(bmin, bmax);
+    output_box = Aabb(box_min, box_max);
     return true;
   }
-  point3 min() const { return bmin; }
-  point3 max() const { return bmax; }
+
+public:
+  point3 box_min;
+  point3 box_max;
+  HittableList sides;
 };
+
+Box::Box(const point3 &p0, const point3 &p1, shared_ptr<Material> ptr) {
+  box_min = p0;
+  box_max = p1;
+
+  sides.add(make_shared<XYRect>(p0.x, p1.x, p0.y, p1.y, p1.y, ptr));
+  sides.add(make_shared<XYRect>(p0.x, p1.x, p0.y, p1.y, p0.y, ptr));
+
+  sides.add(make_shared<XZRect>(p0.x, p1.x, p0.y, p1.y, p1.y, ptr));
+  sides.add(make_shared<XZRect>(p0.x, p1.x, p0.y, p1.y, p0.y, ptr));
+
+  sides.add(make_shared<YZRect>(p0.y, p1.y, p0.y, p1.y, p1.x, ptr));
+  sides.add(make_shared<YZRect>(p0.y, p1.y, p0.y, p1.y, p0.x, ptr));
+}
+
+bool Box::hit(const Ray &r, double t0, double t1, HitRecord &rec) const {
+  return sides.hit(r, t0, t1, rec);
+}
 
 #endif
